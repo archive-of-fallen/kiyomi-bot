8527381c65d7d6a90a222376e6d4d7613fd4c3f7
FallenLight <xFallenLight@users.noreply.github.com>
2018-03-03 19:45:33 +0800

    Please click the '...' if you are interested
    Small improvements to increase compatibility with Heroku

--- src/Kiyomi.js
+++ src/Kiyomi.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const { prefix, token } = require('./config.json');
+const { prefix } = require('./config.json');
 
 client.on('ready', () => {
   console.log('Hey, I\'m ready!');
@@ -13,4 +13,4 @@ client.on('message', msg => {
   if(msg.author.bot) return;
 });
 
-client.login(token);
+client.login(process.env.BOT_TOKEN);
