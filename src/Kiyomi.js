b4e0938dffd61f6bf54e2c358e66e57148d4e0d9
FallenLight <xFallenLight@users.noreply.github.com>
2018-03-18 13:21:30 +0800

    Add: addstatus command
    Functional. However, there's one massive flaw and that is: no data storage. It pushes but when Kiyo is auto-restarted, the data is lost.

--- src/Kiyomi.js
+++ src/Kiyomi.js
@@ -15,6 +15,16 @@ client.on('ready', () => {
 client.on('message', msg => {
   if(!msg.content.startsWith(prefix)) return;
   if(msg.author.bot) return;
+  
+  const args = msg.content.split(' ').slice(1);
+  
+  if(msg.content.startsWith(prefix + 'addstatus')) {
+    if(msg.author.id != '300992784020668416') return;
+    if(!args) {
+      return msg.reply('Please provide some input on what you\'d like me to add.');
+    }
+    statuses.push(args);
+  }
 });
 
 client.login(token);
